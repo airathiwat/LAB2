329802873ce003b64e0b1fbcc8d48d574f5788da
MyNameisDon't_Tell_u_lol <106949683+airathiwat@users.noreply.github.com>
2023-01-10 00:07:35 +0700

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        ชื่อของคุณ:<input type="text" name="name">.<br>
+        อายุของคุณ:<input type="text" name="age">.<br>
+        เพศของคุณ:
+        <input type="radio" name="gender" value="female">female
+        <input type="radio" name="gender" value="male">male<br>
+        สถานะของคุณ:
+        <input type="radio" name="status" value="unmarried">ยังไม่ได้แต่งงาน
+        <input type="radio" name="status" value="married">แต่งงานแล้ว<br>
+        <input type="submit" value="Submit"><br>
+    </form>
+</body>
+</html
