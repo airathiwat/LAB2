444cf0c8f33e41f97d6dffb0d996b42dc2f8dd53
MyNameisDon't_Tell_u_lol <106949683+airathiwat@users.noreply.github.com>
2023-01-11 18:44:13 +0700

    index.php

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html>
-
-<body>
 
     <form action="show.php">
-        ชื่อของคุณ:<input type="text" name="name">.<br>
-        อายุของคุณ:<input type="text" name="age">.<br>
-        เพศของคุณ:
+        YourName?:<input type="text" name="name">.<br>
+        How old are You?:<input type="text" name="age">.<br>
+        Your Gender:
         <input type="radio" name="gender" value="female">female
         <input type="radio" name="gender" value="male">male<br>
-        สถานะของคุณ:
-        <input type="radio" name="status" value="unmarried">ยังไม่ได้แต่งงาน
-        <input type="radio" name="status" value="married">แต่งงานแล้ว<br>
+        Status:
+        <input type="radio" name="status" value="unmarried">unmarried
+        <input type="radio" name="status" value="married">married<br>
         <input type="submit" value="Submit"><br>
     </form>
 </body>
